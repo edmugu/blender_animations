ae616122b53a80eb803b488ec27fa1fb9c3efb1f
Eduardo Munoz-Gutierrez <edmugu@users.noreply.github.com>
2022-10-17 16:49:25 -0700

    Update render.bat

--- video/archery_shot/render.bat
+++ video/archery_shot/render.bat
@@ -1,13 +1,13 @@
 echo off
 REM this renders my archery video
-set mytar=./tmp/video0_original.tar.gz.*
-set myvideo=./tmp/video0_original.MOV
+set mytar=./video_input/video0_original.tar.gz.
+set myvideo=./video_input/video0_original.MOV
 
 REM this is how i compressed the video file in small chunks
-REM tar cvzf - %myvideo% | split --bytes=64MB - ./tmp/video0_original.tar.gz.
+tar cvzf - %myvideo% | split --bytes=64MB - %mytar%
 echo  .
-echo De-compressing the video file
-cat %mytar% | tar xzvf -
+REM echo De-compressing the video file
+REM cat %mytar% | tar xzvf -
 
 echo  .
 echo press enter to exit
