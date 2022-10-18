3fcb2f04fb8637f61da1f97c24274c6eac37df00
Eduardo Munoz-Gutierrez <edmugu@users.noreply.github.com>
2022-10-17 17:02:33 -0700

    fixes the input video compression

--- video/archery_shot/render.bat
+++ video/archery_shot/render.bat
@@ -1,13 +1,14 @@
-echo off
+"C:\Projects\Github\blender_animations\video\archery_shot\video_input\video0_original.MOV"
 REM this renders my archery video
 set mytar=./video_input/video0_original.tar.gz.
 set myvideo=./video_input/video0_original.MOV
 
 REM this is how i compressed the video file in small chunks
-tar cvzf - %myvideo% | split --bytes=64MB - %mytar%
+REM tar cvzf - %myvideo% | split --bytes=64MB - %mytar%
+
 echo  .
-REM echo De-compressing the video file
-REM cat %mytar% | tar xzvf -
+echo De-compressing the video file
+cat ./video_input/video0_original.tar.gz.* | tar xzvf -
 
 echo  .
 echo press enter to exit
